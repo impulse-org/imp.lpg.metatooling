3c6085e15530ede7ad9b8289591ad33e247fb9df
Robert M. Fuhrer <rmfuhrer@optonline.net>
2005-10-31 23:53:45 +0000

    Now does something for the "empty" symbol and literals. Somewhat less offensive color scheme.

--- src/org/eclipse/imp/lpg/editor/TokenColorer.java
+++ src/org/eclipse/imp/lpg/editor/TokenColorer.java
@@ -22,23 +22,34 @@ public class TokenColorer implements ITokenColorer {
 
     static TextAttribute KEYWORD;
 
+    static TextAttribute LITERAL;
+
+    static TextAttribute EMPTY;
+
+
     public TokenColorer() {
 	Display display= Display.getCurrent();
 
 	COMMENT= new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_CYAN), null, SWT.ITALIC);
-
-	SYMBOL= new TextAttribute(display.getSystemColor(SWT.COLOR_RED), null, SWT.NORMAL);
-
+	SYMBOL= new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_RED), null, SWT.NORMAL);
 	KEYWORD= new TextAttribute(display.getSystemColor(SWT.COLOR_BLUE), null, SWT.BOLD);
+	EMPTY= new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_GRAY), null, SWT.BOLD);
+	LITERAL= new TextAttribute(display.getSystemColor(SWT.COLOR_DARK_GRAY), null, SWT.ITALIC);
     }
 
     public TextAttribute getColoring(IParseController controller, IToken token) {
-	if (controller.isKeyword(token.getKind()))
+	if (token.getKind() == JikesPGLexer.TK_EMPTY_KEY)
+	    return EMPTY;
+	else if (controller.isKeyword(token.getKind()))
 	    return KEYWORD;
-	else if (token.getKind() == JikesPGLexer.TK_SYMBOL)
+	else if (token.getKind() == JikesPGLexer.TK_SYMBOL) {
+	    char ch= controller.getLexer().getLexStream().getInputChars()[token.getStartOffset()];
+	    if (ch == '\'' || ch == '"')
+		return LITERAL;
 	    return SYMBOL;
-	else if (token.getKind() == JikesPGLexer.TK_SINGLE_LINE_COMMENT)
+	} else if (token.getKind() == JikesPGLexer.TK_SINGLE_LINE_COMMENT)
 	    return COMMENT;
+
 	return null;
     }
 }
