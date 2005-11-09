868e75d6e6f58693285621445029fe90b47a691b
Robert M. Fuhrer <rmfuhrer@optonline.net>
2005-11-09 03:29:09 +0000

    Fleshed out a bit.

--- src/org/eclipse/imp/lpg/editor/HoverHelper.java
+++ src/org/eclipse/imp/lpg/editor/HoverHelper.java
@@ -4,6 +4,10 @@ import org.eclipse.uide.editor.IHoverHelper;
 import org.eclipse.uide.parser.IASTNodeLocator;
 import org.eclipse.uide.parser.IParseController;
 import org.jikespg.uide.parser.JikesPGParser.ASTNode;
+import org.jikespg.uide.parser.JikesPGParser.Irules;
+import org.jikespg.uide.parser.JikesPGParser.JikesPG;
+import org.jikespg.uide.parser.JikesPGParser.TSYMBOL;
+import org.jikespg.uide.parser.JikesPGParser.rules100;
 
 import com.ibm.lpg.IToken;
 import com.ibm.lpg.PrsStream;
@@ -23,8 +27,27 @@ public class HoverHelper implements IHoverHelper {
 
         ASTNode node= (ASTNode) nodeLocator.findNode(ast, offset);
 
-        if (node == null) return null;
+        if (node == null)
+            return null;
 
-        return new String(parseController.getLexer().getLexStream().getInputChars(), token.getStartOffset(), token.getEndOffset()-token.getStartOffset()+1);
+        if (node instanceof TSYMBOL) {
+            ASTNode def= (ASTNode) findDefOf((TSYMBOL) node, (JikesPG) ast);
+
+            if (def != null)
+                return getSubstring(parseController, ps.getIToken(def.getLeftToken()).getStartOffset(), ps.getIToken(def.getRightToken()).getEndOffset());
+        }
+        return getSubstring(parseController, token);
+    }
+
+    private String getSubstring(IParseController parseController, int start, int end) {
+        return new String(parseController.getLexer().getLexStream().getInputChars(), start, end-start+1);
+    }
+
+    private String getSubstring(IParseController parseController, IToken token) {
+        return getSubstring(parseController, token.getStartOffset(), token.getEndOffset());
+    }
+
+    private Irules findDefOf(TSYMBOL s, JikesPG root) {
+        return null;
     }
 }
