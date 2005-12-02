8ea7713be2e47652e5d6eed2ace1278cf708af93
Robert M. Fuhrer <rmfuhrer@optonline.net>
2005-12-02 20:21:16 +0000

    Removed unnecessary import.

--- src/org/eclipse/imp/lpg/LPGPlugin.java
+++ src/org/eclipse/imp/lpg/LPGPlugin.java
@@ -1,12 +1,12 @@
 package org.jikespg.uide;
 
-import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.uide.runtime.UIDEPluginBase;
 
 public class JikesPGPlugin extends UIDEPluginBase {
     public static final String kPluginID= "org.jikespg.uide";
 
     public JikesPGPlugin() {
+	super();
     }
 
     public String getID() {
