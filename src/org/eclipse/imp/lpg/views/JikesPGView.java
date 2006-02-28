45d432f3266d7703c046d716833293c4f0a4410d
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-02-28 23:06:50 +0000

    Disabled annoying bit of code in openViewDirect() that gives JikesPG Console focus and reorders other views. Reformatted source.

--- src/org/eclipse/imp/lpg/views/JikesPGView.java
+++ src/org/eclipse/imp/lpg/views/JikesPGView.java
@@ -14,82 +14,97 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
 public class JikesPGView extends ViewPart {
-	private Text text;
-	private static JikesPGView singleton;
+    private Text text;
 
-	public JikesPGView() {
-		singleton = this;
-	}
-	
-	public static JikesPGView getDefault() {
-		openView();
-		reset();
-		return singleton;
-	}
+    private static JikesPGView singleton;
 
-	public void createPartControl(Composite parent) {
-		text = new Text(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-		text.setForeground(new Color(parent.getDisplay(), 0,0,255));
-		text.setFont(new Font(parent.getDisplay(), "Helvetica", 10, SWT.NORMAL));
-	}
+    public JikesPGView() {
+	singleton= this;
+    }
 
-	public void setFocus() {
-		text.setFocus();
-	}
+    public static JikesPGView getDefault() {
+	openView();
+	reset();
+	return singleton;
+    }
 
-	public Text getText() {
-		return text;
-	}
+    public void createPartControl(Composite parent) {
+	text= new Text(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
+	text.setForeground(new Color(parent.getDisplay(), 0, 0, 255));
+	text.setFont(new Font(parent.getDisplay(), "Helvetica", 10, SWT.NORMAL));
+    }
 
-	private static void openView() {
-		Display.getDefault().asyncExec(new Runnable() {
-			public void run() {
-				try {
-					openViewDirect();
-				} catch (Throwable e) {
-					e.printStackTrace();
-				}
-			}
+    public void setFocus() {
+	text.setFocus();
+    }
 
-		});
-	}
+    public Text getText() {
+	return text;
+    }
 
-	static void openViewDirect() throws PartInitException {
-		IWorkbench wb = PlatformUI.getWorkbench();
-		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
-		if (win == null) return;
-		IWorkbenchPage page = win.getActivePage();
-		try { page.showView("org.jikespg.uide.views.JikesPGView"); } catch (Exception e) { }
-		try { page.showView("org.eclipse.ui.views.TaskView"); } catch (Exception e) { }
-		try { page.showView("org.eclipse.ui.views.ProblemView"); } catch (Exception e) { }
-	}
+    private static void openView() {
+	Display.getDefault().asyncExec(new Runnable() {
+	    public void run() {
+		try {
+		    openViewDirect();
+		} catch (Throwable e) {
+		    e.printStackTrace();
+		}
+	    }
 
-	public static void println(final String line) {
-		Display.getDefault().asyncExec(new Runnable() {
-			public void run() {
-				try {
-                    if (singleton == null) return;
-					if (singleton.text.isDisposed()) openViewDirect();
-					singleton.text.append(line);
-					singleton.text.append("\n");
-				} catch (Throwable e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-	
-	public static void reset() {
-		Display.getDefault().asyncExec(new Runnable() {
-			public void run() {
-				try {
-                    if (singleton == null) return;
-					if (singleton.text.isDisposed()) openViewDirect();
-					singleton.text.setText("");
-				} catch (Throwable e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	});
+    }
+
+    static void openViewDirect() throws PartInitException {
+//	IWorkbench wb= PlatformUI.getWorkbench();
+//	IWorkbenchWindow win= wb.getActiveWorkbenchWindow();
+//	if (win == null)
+//	    return;
+//	IWorkbenchPage page= win.getActivePage();
+//	try {
+//	    page.showView("org.jikespg.uide.views.JikesPGView");
+//	} catch (Exception e) {
+//	}
+//	try {
+//	    page.showView("org.eclipse.ui.views.TaskView");
+//	} catch (Exception e) {
+//	}
+//	try {
+//	    page.showView("org.eclipse.ui.views.ProblemView");
+//	} catch (Exception e) {
+//	}
+    }
+
+    public static void println(final String line) {
+	Display.getDefault().asyncExec(new Runnable() {
+	    public void run() {
+		try {
+		    if (singleton == null)
+			return;
+		    if (singleton.text.isDisposed())
+			openViewDirect();
+		    singleton.text.append(line);
+		    singleton.text.append("\n");
+		} catch (Throwable e) {
+		    e.printStackTrace();
+		}
+	    }
+	});
+    }
+
+    public static void reset() {
+	Display.getDefault().asyncExec(new Runnable() {
+	    public void run() {
+		try {
+		    if (singleton == null)
+			return;
+		    if (singleton.text.isDisposed())
+			openViewDirect();
+		    singleton.text.setText("");
+		} catch (Throwable e) {
+		    e.printStackTrace();
+		}
+	    }
+	});
+    }
 }
