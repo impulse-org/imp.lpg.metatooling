f54469d627349592c9a6a233a35142dc04a99118
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-03-27 15:25:53 +0000

    StructureMergeViewerCreator now does something more like what other viewer creators do (e.g., it is no longer a Viewer itself).

--- src/org/eclipse/imp/lpg/compare/StructureMergeViewerCreator.java
+++ src/org/eclipse/imp/lpg/compare/StructureMergeViewerCreator.java
@@ -1,47 +1,87 @@
 package org.jikespg.uide;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.eclipse.compare.CompareConfiguration;
 import org.eclipse.compare.IViewerCreator;
+import org.eclipse.compare.ResourceNode;
+import org.eclipse.compare.structuremergeviewer.IStructureComparator;
+import org.eclipse.compare.structuremergeviewer.IStructureCreator;
+import org.eclipse.compare.structuremergeviewer.StructureDiffViewer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.internal.ui.compare.JavaStructureCreator;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.uide.parser.ILexer;
+import org.eclipse.uide.parser.IParser;
+import org.jikespg.uide.parser.JikesPGLexer;
+import org.jikespg.uide.parser.JikesPGParser;
+import org.jikespg.uide.parser.JikesPGParser.ASTNode;
 
-public class StructureMergeViewerCreator extends Viewer implements IViewerCreator {
-    public StructureMergeViewerCreator() {
-	super();
-	// TODO Auto-generated constructor stub
+public class StructureMergeViewerCreator implements IViewerCreator {
+    static class ASTWrapper implements IStructureComparator {
+	ASTNode node;
+	public ASTWrapper(ASTNode n) {
+	    node= n;
+	}
+	public Object[] getChildren() {
+	    return node.getChildren();
+	}
     }
-
-    public Control getControl() {
-	// TODO Auto-generated method stub
-	return null;
-    }
-
-    public Object getInput() {
-	// TODO Auto-generated method stub
-	return null;
-    }
-
-    public ISelection getSelection() {
-	// TODO Auto-generated method stub
-	return null;
-    }
-
-    public void refresh() {
-    // TODO Auto-generated method stub
-    }
-
-    public void setInput(Object input) {
-    // TODO Auto-generated method stub
-    }
-
-    public void setSelection(ISelection selection, boolean reveal) {
-    // TODO Auto-generated method stub
-    }
-
     public Viewer createViewer(Composite parent, CompareConfiguration config) {
-	// TODO Auto-generated method stub
-	return null;
+	return new StructureDiffViewer(parent, config) {
+	    IStructureCreator fStructureCreator;
+	    {
+		fStructureCreator= new IStructureCreator() {
+		    public String getName() {
+			return "yo";
+		    }
+		    public IStructureComparator getStructure(Object input) {
+			try {
+			    if (input instanceof ResourceNode) {
+				ResourceNode resNode= (ResourceNode) input;
+				IResource res= resNode.getResource();
+				if (res instanceof IFile) {
+				    IFile file= (IFile) res;
+
+				    ILexer lexer= new JikesPGLexer();
+				    IParser parser= new JikesPGParser(lexer.getLexStream());
+				    InputStream in= file.getContents();
+				    StringBuffer buff= new StringBuffer();
+				    byte ch[] = new byte[1];
+				    while (in.read(ch) > 0)
+					buff.append((char) ch[0]);
+				    char[] contentsArray = buff.toString().toCharArray();
+
+				    lexer.initialize(contentsArray, file.getLocation().toString());
+				    parser.getParseStream().resetTokenStream();
+				    lexer.lexer(null, parser.getParseStream()); // Lex the stream to produce the token stream
+
+				    return new ASTWrapper((ASTNode) parser.parser(null, 0));
+				}
+			    }
+			} catch (IOException io) {
+			} catch (CoreException ce) {
+			}
+			return null;
+		    }
+		    public IStructureComparator locate(Object path, Object input) {
+			return null;
+		    }
+		    public String getContents(Object node, boolean ignoreWhitespace) {
+			return null;
+		    }
+		    public void save(IStructureComparator node, Object input) {
+			// TODO Auto-generated method stub
+		    }
+		};
+		setStructureCreator(fStructureCreator);
+	    }
+	};
     }
 }
