3efa191a3f58e52c0911bd7329a7f3afb59a6922
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-04-19 23:28:48 +0000

    Now actually does something! (Shows the body of non-terminals.)

--- src/org/eclipse/imp/lpg/editor/HoverHelper.java
+++ src/org/eclipse/imp/lpg/editor/HoverHelper.java
@@ -1,16 +1,20 @@
 package org.jikespg.uide.editor;
 
+import lpg.lpgjavaruntime.IToken;
+import lpg.lpgjavaruntime.PrsStream;
 import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.uide.editor.IHoverHelper;
 import org.eclipse.uide.parser.IASTNodeLocator;
 import org.eclipse.uide.parser.IParseController;
 import org.jikespg.uide.parser.JikesPGParser.ASTNode;
 import org.jikespg.uide.parser.JikesPGParser.IASTNodeToken;
+import org.jikespg.uide.parser.JikesPGParser.IJikesPG_item;
 import org.jikespg.uide.parser.JikesPGParser.JikesPG;
+import org.jikespg.uide.parser.JikesPGParser.JikesPG_itemList;
+import org.jikespg.uide.parser.JikesPGParser.RulesSeg;
 import org.jikespg.uide.parser.JikesPGParser.nonTerm;
-
-import lpg.lpgjavaruntime.IToken;
-import lpg.lpgjavaruntime.PrsStream;
+import org.jikespg.uide.parser.JikesPGParser.nonTermList;
+import org.jikespg.uide.parser.JikesPGParser.rules_segment;
 
 public class HoverHelper implements IHoverHelper {
     public String getHoverHelpAt(IParseController parseController, ISourceViewer srcViewer, int offset) {
@@ -48,6 +52,33 @@ public class HoverHelper implements IHoverHelper {
     }
 
     private nonTerm findDefOf(IASTNodeToken s, JikesPG root) {
-        return null;
+	// This would use the auto-generated bindings if they were implemented already...
+	String id= stripName(s.toString());
+	JikesPG_itemList itemList= root.getJikesPG_INPUT();
+
+	for(int i=0; i < itemList.size(); i++) {
+	    IJikesPG_item item= itemList.getJikesPG_itemAt(i);
+
+	    if (!(item instanceof RulesSeg))
+		continue;
+	    RulesSeg rules= (RulesSeg) item;
+	    rules_segment rulesSeg= rules.getrules_segment();
+	    nonTermList nonTermList= rulesSeg.getnonTermList();
+
+	    for(int j=0; j < nonTermList.size(); j++) {
+		nonTerm nonTerm= nonTermList.getnonTermAt(j);
+		String nonTermName= stripName(nonTerm.getSYMBOL().toString());
+
+		if (nonTermName.equals(id))
+		    return nonTerm;
+	    }
+	}
+	return null;
+    }
+
+    private String stripName(String rawId) {
+	int idx= rawId.indexOf('$');
+
+	return (idx >= 0) ? rawId.substring(0, idx) : rawId;
     }
 }
