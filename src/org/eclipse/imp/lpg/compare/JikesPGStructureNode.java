0bbffc6aeb3cab3e42599364aaeaa1346590b465
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-04-25 19:57:31 +0000

    Extracted nested class GetChildrenVisitor to its own compilation unit.

--- src/org/eclipse/imp/lpg/compare/JikesPGStructureNode.java
+++ src/org/eclipse/imp/lpg/compare/JikesPGStructureNode.java
@@ -12,11 +12,10 @@ import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.swt.graphics.Image;
 import org.jikespg.uide.IJikesPGResources;
 import org.jikespg.uide.JikesPGPlugin;
+import org.jikespg.uide.parser.GetChildrenVisitor;
 import org.jikespg.uide.parser.JikesPGParser.*;
 
 public class JikesPGStructureNode extends DocumentRangeNode implements ITypedElement {
-    private final static Object[] NO_CHILDREN= new Object[0];
-
     public final static int OPTION= 0;
     public final static int BODY= 1;
     public static final int ACTION= 2;
@@ -42,93 +41,6 @@ public class JikesPGStructureNode extends DocumentRangeNode implements ITypedEle
     private static final char INCLUDE_PREFIX= 'I';
     private static final char ALIAS_PREFIX= 'L';
     private static final char NONTERMINAL_PREFIX= 'N';
-
-    /**
-     * A simple visitor that produces the immediate children of a given AST node, each
-     * wrapped by a JikesPGStructureNode.<br>
-     * The implementation below actually omits certain uninteresting intermediate nodes
-     * from the structure.
-     */
-    private static class GetChildrenVisitor extends AbstractVisitor implements Visitor {
-        private Object[] fChildren= NO_CHILDREN;
-
-        private final JikesPGStructureNode fStructureNode;
-
-        public GetChildrenVisitor(JikesPGStructureNode node) {
-            fStructureNode= node;
-        }
-
-        public void unimplementedVisitor(String s) { }
-
-        public boolean visit(JikesPG n) {
-            options_segment options= n.getoptions_segment();
-            JikesPG_itemList itemList= n.getJikesPG_INPUT();
-            int count= 1 + itemList.size();
-
-            fChildren= new Object[count];
-            fChildren[0]= new JikesPGStructureNode(options, fStructureNode, OPTION, "options");
-
-            for(int i= 0; i < itemList.size(); i++) {
-                IJikesPG_item item= itemList.getJikesPG_itemAt(i);
-
-                fChildren[i+1]= new JikesPGStructureNode((ASTNode) item, fStructureNode, BODY, item.getLeftIToken().toString());
-            }
-            return false;
-        }
-
-        public boolean visit(GlobalsSeg n) {
-            globals_segment1 globals= (globals_segment1) n.getglobals_segment();
-            fChildren= new Object[] {
-                    new JikesPGStructureNode(globals.getaction_segment(), fStructureNode, GLOBAL, "global")
-            };
-            return false;
-        }
-
-        public boolean visit(DefineSeg n) {
-            define_segment1 defSeg= (define_segment1) n.getdefine_segment();
-            int count=1;
-            for(define_segment1 ds= defSeg; ds != null && ds.getdefine_segment() instanceof define_segment1; ds= (define_segment1) ds.getdefine_segment()) {
-                count++;
-            }
-            fChildren= new Object[count];
-            int i=0;
-            for(define_segment1 ds= defSeg; ds != null; ds= (define_segment1) ds.getdefine_segment(), i++) {
-                fChildren[i]= new JikesPGStructureNode(ds, fStructureNode, DEFINE, ds.getmacro_name_symbol().toString());
-                if (!(ds.getdefine_segment() instanceof define_segment1))
-                    break;
-            }
-            return false;
-        }
-
-        public boolean visit(TerminalsSeg n) {
-            terminalList termList= (terminalList) n.getterminals_segment();
-            int N= termList.size();
-
-            fChildren= new Object[N];
-            for(int i=0; i < N; i++) {
-                terminal term= (terminal) termList.getElementAt(i);
-                fChildren[N-i-1]= new JikesPGStructureNode(term, fStructureNode, TERMINAL, term.getterminal_symbol().toString());
-            }
-            return false;
-        }
-
-        public boolean visit(RulesSeg n) {
-            rules_segment rulesSegment= n.getrules_segment();
-            nonTermList nonTermList= rulesSegment.getnonTermList();
-            int N= nonTermList.size();
-
-            fChildren= new Object[N];
-            for(int i=0; i < N; i++) {
-                nonTerm nt= (nonTerm) nonTermList.getElementAt(i);
-                fChildren[N-i-1]= new JikesPGStructureNode(nt, fStructureNode, NONTERMINAL, nt.getSYMBOL().toString());
-            }
-            return false;
-        }
-
-        public Object[] getChildren() {
-            return fChildren;
-        }
-    }
 
     private ASTNode fASTNode;
 
