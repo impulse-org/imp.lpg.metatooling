cb59e0354b644dc9e38b0afd531771df32242b55
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-04-25 19:54:27 +0000

    Now handles macro definitions. Extracted findDefOf() to ASTUtils, where it will do more good.

--- src/org/eclipse/imp/lpg/editor/HoverHelper.java
+++ src/org/eclipse/imp/lpg/editor/HoverHelper.java
@@ -1,6 +1,5 @@
 package org.jikespg.uide.editor;
 
-import java.util.List;
 import lpg.lpgjavaruntime.IToken;
 import lpg.lpgjavaruntime.PrsStream;
 import org.eclipse.jface.text.source.ISourceViewer;
@@ -10,14 +9,7 @@ import org.eclipse.uide.parser.IParseController;
 import org.jikespg.uide.parser.ASTUtils;
 import org.jikespg.uide.parser.JikesPGParser.ASTNode;
 import org.jikespg.uide.parser.JikesPGParser.IASTNodeToken;
-import org.jikespg.uide.parser.JikesPGParser.IJikesPG_item;
 import org.jikespg.uide.parser.JikesPGParser.JikesPG;
-import org.jikespg.uide.parser.JikesPGParser.JikesPG_itemList;
-import org.jikespg.uide.parser.JikesPGParser.RulesSeg;
-import org.jikespg.uide.parser.JikesPGParser.nonTerm;
-import org.jikespg.uide.parser.JikesPGParser.nonTermList;
-import org.jikespg.uide.parser.JikesPGParser.rules_segment;
-import org.jikespg.uide.parser.JikesPGParser.terminal;
 
 public class HoverHelper implements IHoverHelper {
     public String getHoverHelpAt(IParseController parseController, ISourceViewer srcViewer, int offset) {
@@ -38,7 +30,7 @@ public class HoverHelper implements IHoverHelper {
             return null;
 
         if (node instanceof IASTNodeToken) {
-            ASTNode def= (ASTNode) findDefOf((IASTNodeToken) node, (JikesPG) ast);
+            ASTNode def= (ASTNode) ASTUtils.findDefOf((IASTNodeToken) node, (JikesPG) ast);
 
             if (def != null)
                 return getSubstring(parseController, def.getLeftIToken().getStartOffset(), def.getRightIToken().getEndOffset());
@@ -52,30 +44,6 @@ public class HoverHelper implements IHoverHelper {
 
     public static String getSubstring(IParseController parseController, IToken token) {
         return getSubstring(parseController, token.getStartOffset(), token.getEndOffset());
-    }
-
-    public static ASTNode findDefOf(IASTNodeToken s, JikesPG root) {
-	// This would use the auto-generated bindings if they were implemented already...
-	String id= stripName(s.toString());
-
-        List/*<nonTerm>*/ nonTermList= ASTUtils.getNonTerminals(root);
-        List/*<terminal>*/ termList= ASTUtils.getTerminals(root);
-	
-        for(int j=0; j < nonTermList.size(); j++) {
-            nonTerm nonTerm= (nonTerm) nonTermList.get(j);
-            String nonTermName= stripName(nonTerm.getSYMBOL().toString());
-
-            if (nonTermName.equals(id))
-        	return nonTerm;
-        }
-        for(int j=0; j < termList.size(); j++) {
-            terminal term= (terminal) termList.get(j);
-            String termName= stripName(term.getterminal_symbol().toString());
-
-            if (termName.equals(id))
-        	return term;
-        }
-	return null;
     }
 
     public static String stripName(String rawId) {
