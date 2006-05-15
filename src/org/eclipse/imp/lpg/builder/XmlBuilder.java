84a9c3321e71e73263696459e8f4d1509504978f
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-05-15 23:38:52 +0000

    Reformatted.

--- src/org/eclipse/imp/lpg/builder/XmlBuilder.java
+++ src/org/eclipse/imp/lpg/builder/XmlBuilder.java
@@ -19,64 +19,66 @@ import org.eclipse.core.runtime.IProgressMonitor;
  * @author CLaffra
  */
 public class XmlBuilder extends IncrementalProjectBuilder {
-	
-	static String lpg;
-	static String outdir;
-	static String incdir;
-	
-	protected IProject[] build(int kind, Map args, IProgressMonitor monitor) {
-		if (kind == IncrementalProjectBuilder.FULL_BUILD) {
-			fullBuild(monitor);
-		} else {
-			IResourceDelta delta = getDelta(getProject());
-			if (delta == null) {
-				fullBuild(monitor);
-			} else {
-				incrementalBuild(delta, monitor);
+
+    static String lpg;
+
+    static String outdir;
+
+    static String incdir;
+
+    protected IProject[] build(int kind, Map args, IProgressMonitor monitor) {
+	if (kind == IncrementalProjectBuilder.FULL_BUILD) {
+	    fullBuild(monitor);
+	} else {
+	    IResourceDelta delta= getDelta(getProject());
+	    if (delta == null) {
+		fullBuild(monitor);
+	    } else {
+		incrementalBuild(delta, monitor);
+	    }
+	}
+	return null;
+    }
+
+    private void incrementalBuild(IResourceDelta delta, IProgressMonitor monitor) {
+	try {
+	    delta.accept(new IResourceDeltaVisitor() {
+		public boolean visit(IResourceDelta delta) {
+		    IResource resource= delta.getResource();
+		    if (!resource.getProject().equals(getProject()))
+			return false;
+		    IPath path= resource.getRawLocation();
+		    IProject project= resource.getProject();
+		    if (path != null) {
+			String fileName= path.toString();
+			if (fileName.indexOf("/bin/") != -1 && "xml".equals(path.getFileExtension())) {
+			    try {
+				processXmlFile(project, fileName);
+				project.refreshLocal(IResource.DEPTH_INFINITE, null);
+			    } catch (Exception e) {
+				e.printStackTrace();
+			    }
 			}
+		    }
+		    return true; // visit children too
 		}
-		return null;
+	    });
+	} catch (Exception e) {
+	    e.printStackTrace();
 	}
+    }
 
-	private void incrementalBuild(IResourceDelta delta, IProgressMonitor monitor) {
-		try {
-			delta.accept(new IResourceDeltaVisitor() {
-				public boolean visit(IResourceDelta delta) {
-					IResource resource = delta.getResource();
-                    if (!resource.getProject().equals(getProject()))
-                        return false;
-					IPath path = resource.getRawLocation();
-                    IProject project = resource.getProject();
-					if (path != null) {
-						String fileName = path.toString();
-						if (fileName.indexOf("/bin/") != -1 && "xml".equals(path.getFileExtension())) {
-							try {
-								processXmlFile(project, fileName);
-								project.refreshLocal(IResource.DEPTH_INFINITE, null);
-							} catch (Exception e) {
-								e.printStackTrace();
-							}
-						}
-					}
-					return true; // visit children too
-				}
-			});
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+    protected void processXmlFile(IProject project, final String fileName) {
+	try {
+	    System.out.println("XML builder runs on " + getProject());
+	    System.out.println("Process LPG output in " + fileName);
+	    XmlOutputParser.parse(project, fileName);
+	} catch (final Exception e) {
+	    e.printStackTrace();
 	}
+    }
 
-	protected void processXmlFile(IProject project, final String fileName) {
-		try {
-            System.out.println("XML builder runs on "+getProject());
-            System.out.println("Process LPG output in "+fileName);
-			XmlOutputParser.parse(project, fileName);
-		} catch (final Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private void fullBuild(IProgressMonitor monitor) {
-		System.out.println("full XML build");  // TODO: implement full XML builder
-	}
+    private void fullBuild(IProgressMonitor monitor) {
+	System.out.println("full XML build"); // TODO: implement full XML builder
+    }
 }
