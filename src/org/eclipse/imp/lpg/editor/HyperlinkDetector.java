5ebc8f4ec07dcf4a59a318f507dc056d9c90ffcc
Stanley M. Sutton <suttons@us.ibm.com>
2006-07-17 19:17:57 +0000

    Updated version that uses an updated version of the hyperlink-detector template and the new reference-resolver component.

--- src/org/eclipse/imp/lpg/editor/HyperlinkDetector.java
+++ src/org/eclipse/imp/lpg/editor/HyperlinkDetector.java
@@ -1,80 +1,88 @@
 package org.jikespg.uide.editor;
 
-import lpg.lpgjavaruntime.IToken;
-import lpg.lpgjavaruntime.PrsStream;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.ITextViewer;
 import org.eclipse.jface.text.Region;
 import org.eclipse.jface.text.hyperlink.IHyperlink;
 import org.eclipse.uide.core.ILanguageService;
+import org.eclipse.uide.editor.IReferenceResolver;
 import org.eclipse.uide.editor.ISourceHyperlinkDetector;
 import org.eclipse.uide.parser.IASTNodeLocator;
 import org.eclipse.uide.parser.IParseController;
-import org.jikespg.uide.parser.ASTUtils;
-import org.jikespg.uide.parser.JikesPGParser.ASTNode;
-import org.jikespg.uide.parser.JikesPGParser.IASTNodeToken;
-import org.jikespg.uide.parser.JikesPGParser.Imacro_name_symbol;
-import org.jikespg.uide.parser.JikesPGParser.JikesPG;
-import org.jikespg.uide.parser.JikesPGParser.nonTerm;
-import org.jikespg.uide.parser.JikesPGParser.terminal;
+
+
+/**
+ * Provides a method to detect hyperlinks originating from a
+ * given region in the parse stream of a given parse controller.
+ */
 
 public class HyperlinkDetector implements ISourceHyperlinkDetector, ILanguageService {
+	
     public HyperlinkDetector() { }
 
-    public IHyperlink[] detectHyperlinks(final ITextViewer textViewer, final IRegion region, IParseController parseController) {
-        int offset= region.getOffset();
-        PrsStream ps= parseController.getParser().getParseStream();
-        IToken token= ps.getTokenAtCharacter(offset);
-
-        if (token == null) return null;
-
-        ASTNode ast= (ASTNode) parseController.getCurrentAst();
-
+    public IHyperlink[] detectHyperlinks(
+    		final ITextViewer textViewer, final IRegion region, IParseController parseController)
+    {
+    	// This is the only language-specific bit ...
+    	// Note:  JikesPG should be the name of the language
+    	// represented typographically like a class name
+    	IReferenceResolver linkMapper = new JikesPGReferenceResolver();
+    	
+    	// Get stuff for getting link source node
+        Object ast= parseController.getCurrentAst();
         if (ast == null) return null;
+        int offset= region.getOffset();
+        IASTNodeLocator nodeLocator = parseController.getNodeLocator();
 
-        IASTNodeLocator nodeLocator= parseController.getNodeLocator();
+        // Get link source node
+        Object source = nodeLocator.findNode(ast, offset);
+        if (source == null) return null;
+        //if (!(source instanceof IASTNodeToken)) return null;
+        if (!linkMapper.hasSuitableLinkSourceType(source)) return null;
 
-        ASTNode node= (ASTNode) nodeLocator.findNode(ast, offset);
+        // Got a suitable link source node; get link target node
+       	final Object target = linkMapper.getLinkTarget(source, parseController);
+       	if (target == null) return null;
 
-        if (node == null)
-            return null;
-
-        if (node instanceof IASTNodeToken) {
-            final ASTNode def= ASTUtils.findDefOf((IASTNodeToken) node, (JikesPG) ast);
-
-            if (def != null) {
-                final int srcStart= node.getLeftIToken().getStartOffset();
-                final int srcLength= node.getRightIToken().getEndOffset() - srcStart + 1;
-                final int targetStart= def.getLeftIToken().getStartOffset();
-                // SMS 2 Jun 2006:  replaced original calculation of target length
-                // (which was erroneously based on node rather than def)
-                final int targetLength= def.getRightIToken().getEndOffset() - targetStart + 1;
-
-                return new IHyperlink[] {
-                        new IHyperlink() {
-                            public IRegion getHyperlinkRegion() {
-                                return new Region(srcStart, srcLength);
-                            }
-                            public String getTypeLabel() {
-                        	if (def instanceof nonTerm)
-                        	    return "non-terminal";
-                        	if (def instanceof terminal)
-                        	    return "terminal";
-                        	if (def instanceof Imacro_name_symbol)
-                        	    return "macro";
-                        	return null;
-                            }
-                            public String getHyperlinkText() {
-                                return def.getLeftIToken().toString();
-                            }
-                            public void open() {
-                                textViewer.setSelectedRange(targetStart, targetLength);
-                                textViewer.revealRange(targetStart, targetLength);
-                            }
-                        }
-                };
+        // Link target node exists; get info for new hyperlink
+       	// Note:  source presumably has a legitimate starting offset
+       	// and length (since they have been selected from the source file)
+        final int srcStart= nodeLocator.getStartOffset(source);
+        final int srcLength= nodeLocator.getEndOffset(source) - srcStart + 1;
+        // But the target (depending on what--and where--the target is)
+        // may not have a legitimate location (or one wihtin the file).
+        // In that case, set the target to the beginning of the file
+        // and give it a nominal length.
+        final int targetStart;
+        if (nodeLocator.getStartOffset(target) < 0) targetStart = 0;
+        else targetStart = nodeLocator.getStartOffset(target);
+        final int targetLength;
+        if (targetStart == 0) targetLength = 1;
+        else targetLength = nodeLocator.getEndOffset(target) - targetStart + 1;
+        // Get the link text
+        final String linkText = linkMapper.getLinkText(source);
+       	
+        // Create and return the new hyperlink
+        IHyperlink[] result = new IHyperlink[] {
+            new IHyperlink() {
+                public IRegion getHyperlinkRegion() {
+                    return new Region(srcStart, srcLength);
+                }
+                public String getTypeLabel() {
+               	    return target.getClass().getName();
+                }
+                public String getHyperlinkText() {
+                    return new String(linkText);
+                }
+                public void open() {
+                    textViewer.setSelectedRange(targetStart, targetLength);
+                    textViewer.revealRange(targetStart, targetLength);
+                }
             }
-        }
-        return null;
+        };
+        
+        return result;
     }
+    
 }
+
