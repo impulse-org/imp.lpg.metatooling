e441a30f1c33ce6dc684c66ecf0398770a6bc903
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-07-19 15:00:22 +0000

    Removed in deference to JikesPGLabelProvider, which does essentially the same thing.

--- src/org/eclipse/imp/lpg/editor/JikesPGElementLabels.java
+++
@@ -1,9 +0,0 @@
-package org.jikespg.uide.editor;
-
-public class JikesPGElementLabels {
-
-    public static String getTextLabel(Object element, long flags) {
-	// TODO Auto-generated method stub
-	return "?huh?";
-    }
-}
