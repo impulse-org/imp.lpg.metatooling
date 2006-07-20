4e3d35f515a170d1ae3ec359ddaa027204645243
Robert M. Fuhrer <rmfuhrer@optonline.net>
2006-07-20 22:39:56 +0000

    Trimmed some dead methods and fields. Hoisted hasChildren() up to the base class OutlineContentProvider.

--- src/org/eclipse/imp/lpg/editor/JikesPGContentProvider.java
+++ src/org/eclipse/imp/lpg/editor/JikesPGContentProvider.java
@@ -18,9 +18,6 @@ import org.jikespg.uide.parser.JikesPGParser.option_value0;
 import org.jikespg.uide.parser.JikesPGParser.rhsList;
 
 public class JikesPGContentProvider extends OutlineContentProvider implements ILanguageService {
-    protected boolean fProvideMembers;
-    protected boolean fProvideWorkingCopy;
-
     public JikesPGContentProvider() {
 	this(false, null);
     }
@@ -33,68 +30,6 @@ public class JikesPGContentProvider extends OutlineContentProvider implements IL
      */
     public JikesPGContentProvider(boolean provideMembers, OutlineInformationControl oic) {
 	super(oic, true);
-	fProvideMembers= provideMembers;
-	fProvideWorkingCopy= provideMembers;
-    }
-
-    /**
-     * Returns whether members are provided when asking
-     * for a compilation units or class file for its children.
-     * 
-     * @return <code>true</code> if the content provider provides members; 
-     * otherwise <code>false</code> is returned
-     */
-    public boolean getProvideMembers() {
-	return fProvideMembers;
-    }
-
-    /**
-     * Sets whether the content provider is supposed to return members
-     * when asking a compilation unit or class file for its children.
-     * 
-     * @param b if <code>true</code> then members are provided. 
-     * If <code>false</code> compilation units and class files are the
-     * leaves provided by this content provider.
-     */
-    public void setProvideMembers(boolean b) {
-	fProvideMembers= b;
-    }
-
-    /**
-     * @deprecated Since 3.0 compilation unit children are always provided as working copies. The Java model
-     * does not support the 'original' mode anymore. 
-     */
-    public boolean getProvideWorkingCopy() {
-	return fProvideWorkingCopy;
-    }
-
-    /**
-     * @deprecated Since 3.0 compilation unit children are always provided from the working copy. The Java model
-     * offers a unified world and does not support the 'original' mode anymore. 
-     */
-    public void setProvideWorkingCopy(boolean b) {
-	fProvideWorkingCopy= b;
-    }
-
-    /* (non-Javadoc)
-     * @see IWorkingCopyProvider#providesWorkingCopies()
-     */
-    public boolean providesWorkingCopies() {
-	return getProvideWorkingCopy();
-    }
-
-    /* (non-Javadoc)
-     * Method declared on IStructuredContentProvider.
-     */
-    public Object[] getElements(Object parent) {
-	return getChildren(parent);
-    }
-
-    /* (non-Javadoc)
-     * Method declared on IContentProvider.
-     */
-    public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-	super.inputChanged(viewer, oldInput, newInput);
     }
 
     /* (non-Javadoc)
@@ -115,15 +50,6 @@ public class JikesPGContentProvider extends OutlineContentProvider implements IL
 		iter.remove();
 	}
 	return children.toArray();
-    }
-
-    /* (non-Javadoc)
-     * @see ITreeContentProvider
-     */
-    public boolean hasChildren(Object element) {
-	Object[] children= getChildren(element);
-
-	return (children != null) && children.length > 0;
     }
 
     /* (non-Javadoc)
