6b8834bd7f1633b7aa3a14883c1cc3999604e063
Stanley M. Sutton <suttons@us.ibm.com>
2006-10-13 16:44:50 +0000

    Incorporates reference to new language-specific generated node locator.

--- templates/ParseController.java
+++ templates/ParseController.java
@@ -70,8 +70,8 @@ public class $CLASS_NAME_PREFIX$ParseController implements IParseController
     }
     public IToken getTokenAtCharacter(int offset) {
     	return parser.getParseStream().getTokenAtCharacter(offset);
-    }
-    public IASTNodeLocator getNodeLocator() { return new AstLocator(); }
+    }											// SMS 3 Oct 2006:  trying new locator
+    public IASTNodeLocator getNodeLocator() { return new $CLASS_NAME_PREFIX$NodeLocator(); }	//return new AstLocator(); }
 
     public boolean hasErrors() { return currentAst == null; }
     public List getErrors() { return Collections.singletonList(new ParseError("parse error", null)); }
