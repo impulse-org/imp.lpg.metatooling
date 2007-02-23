34a5ecaac922c796a68d61c5d746259e3512e435
Stanley M. Sutton <suttons@us.ibm.com>
2007-02-23 17:01:20 +0000

    Changes reflecting the new name of the lpg runtime.

--- src/org/eclipse/imp/lpg/wizards/NewUIDEParserWizard.java
+++ src/org/eclipse/imp/lpg/wizards/NewUIDEParserWizard.java
@@ -60,7 +60,8 @@ public class NewUIDEParserWizard extends ExtensionPointWizard implements INewWiz
         // PC Restored on 10/16/2006. This is a critical dependency.
         // Eclipse forbids introducing project dependencies on plugins.
         //
-	dependencies.add("lpg");
+	// SMS 23 Feb 2007:  leg -> leg.runtime
+	dependencies.add("lpg.runtime");
     }
 
     protected List getPluginDependencies() {
