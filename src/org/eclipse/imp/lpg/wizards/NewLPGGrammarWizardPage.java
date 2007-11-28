899eed89548da2b6c02776fe2650969e5117385a
Stanley M. Sutton <suttons@us.ibm.com>
2007-11-28 20:47:56 +0000

    Added methods getViewerFilterForProjects() and getSelectionValidatorForProjects() to override corresponding methods in IMPWizardPage so as to filter and validate Java projects rather than IDE projects.
    Created a new method adjustLanguageByProject(IProject, WizardPageField) to control the value and enabled state of the language field according to whether the selected project is an IDE project or not.  Added calls to this method in places where the language field is set.
    
    Updated imports.

--- src/org/eclipse/imp/lpg/wizards/NewLPGGrammarWizardPage.java
+++ src/org/eclipse/imp/lpg/wizards/NewLPGGrammarWizardPage.java
@@ -5,13 +5,19 @@
  */
 package org.eclipse.imp.lpg.wizards;
 
+import org.eclipse.core.resources.IProject;
 import org.eclipse.imp.core.ErrorHandler;
-import org.eclipse.imp.runtime.RuntimePlugin;
+import org.eclipse.imp.ui.dialogs.filters.ViewerFilterForJavaProjects;
+import org.eclipse.imp.ui.dialogs.validators.SelectionValidatorForJavaProjects;
+import org.eclipse.imp.utils.ValidationUtils;
 import org.eclipse.imp.wizards.GeneratedComponentAttribute;
 import org.eclipse.imp.wizards.GeneratedComponentWizard;
+import org.eclipse.imp.wizards.WizardPageField;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.dialogs.ISelectionValidator;
 
 /**
  * The "New" wizard page allows setting the container for the new file as well as the
@@ -46,6 +52,10 @@ public class NewLPGGrammarWizardPage extends NewLanguageSupportWizardPage
 		super.createControl(parent);
 		// Possible (or desirable?) here to set language even if not empty?
 		setLanguageIfEmpty();
+		
+		adjustLanguageByProject(fProject, getField("Language"));
+
+
 		try {
 	
 			// Don't worry about changing the Package field if the language
@@ -53,19 +63,46 @@ public class NewLPGGrammarWizardPage extends NewLanguageSupportWizardPage
 			// name and the target package for the grammar can vary independently
 	        
 		    fProjectText.addModifyListener(new ModifyListener() {
-			public void modifyText(ModifyEvent e) {
-				// SMS 13 Jun 2007
-			    //setLanguageIfEmpty();
-				// Need a new language with a new project	
-				setLanguage();
-				// Clear target package for grammar; it must change whent
-				// the project changes, but we don't presume a default value
-				getField("Package").setText("");
-			}
+				public void modifyText(ModifyEvent e) {
+					// SMS 13 Jun 2007
+				    //setLanguageIfEmpty();
+					// Need a new language with a new project	
+					//setLanguage();
+					
+					adjustLanguageByProject(fProject, getField("Language"));
+						
+					// Clear target package for grammar; it must change whent
+					// the project changes, but we don't presume a default value
+					getField("Package").setText("");
+				}
 		    });
 		} catch (Exception e) {
 		    ErrorHandler.reportError("NewLPGGrammarWizardPage.createControl(..):  Internal error, extension point schema may have changed", e);
 		}
     }
+    
+    
+	// SMS 28 Nov 2007
+    protected void adjustLanguageByProject(IProject project, WizardPageField languageField)
+    {
+    	if (project != null) {
+    		if (ValidationUtils.isIDEProject(project)) {
+    			setLanguageIfEmpty();
+    			languageField.setEnabled(false);
+    		} else {
+    			languageField.setText("");
+    			languageField.setEnabled(true);
+    		}
+    	}	
+    }
 
+    
+    protected ViewerFilter getViewerFilterForProjects() {
+    	return new ViewerFilterForJavaProjects();
+    }
+
+	protected ISelectionValidator getSelectionValidatorForProjects() {
+		return new SelectionValidatorForJavaProjects();
+	}
+    
 }
