155bcfe964cf3061ffaeab73a81ec162527cf8d1
Stanley M. Sutton <suttons@us.ibm.com>
2007-12-18 20:19:25 +0000

    Updated templates as part of process of reducing IMP reliance on LPG types.  Based on example types worked out by RMF for LEG.

--- templates/ASTNodeLocator.java
+++ templates/ASTNodeLocator.java
@@ -1,16 +1,15 @@
 package $PACKAGE_NAME$;
 
-import $AST_PKG_NODE$;
-
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 
-import org.eclipse.imp.parser.IASTNodeLocator;
-
+import $PACKAGE_NAME$.Ast.ASTNode;
 import $PACKAGE_NAME$.Ast.AbstractVisitor;
+import org.eclipse.imp.parser.ISourcePositionLocator;
+
 import lpg.runtime.*;
 
-public class $CLASS_NAME_PREFIX$ASTNodeLocator implements IASTNodeLocator
+public class $CLASS_NAME_PREFIX$ASTNodeLocator implements ISourcePositionLocator
 {
     private final Object[] fNode= new Object[1];
 
@@ -19,69 +18,78 @@ public class $CLASS_NAME_PREFIX$ASTNodeLocator implements IASTNodeLocator
 
     public $CLASS_NAME_PREFIX$ASTNodeLocator( ) {
     }
+	private final class NodeVisitor extends AbstractVisitor {
+		public void unimplementedVisitor(String s) {
+			// System.out.println("NodeVisitor.unimplementedVisitor:  Unimplemented");
+		}
 
-    private final class NodeVisitor extends AbstractVisitor {
-        public void unimplementedVisitor(String s) {
-            // System.out.println("NodeVisitor.unimplementedVisitor:  Unimplemented");
-        }
+		public boolean preVisit(IAst element) {
+			int nodeStartOffset = element.getLeftIToken().getStartOffset();
+			int nodeEndOffset = element.getRightIToken().getEndOffset();
+			//System.out.println("LEGNodeLocator.NodeVisitor.preVisit(ASTNode):  Examining " + element.getClass().getName() +
+			//    " @ [" + nodeStartOffset + "->" + nodeEndOffset + ']');
 
-        public boolean preVisit(IAst element)
-        {
-            int nodeStartOffset = element.getLeftIToken().getStartOffset();
-            int nodeEndOffset = element.getRightIToken().getEndOffset();
-            //System.out.println("$LANG_NAME$NodeLocator.NodeVisitor.preVisit($AST_NODE$):  Examining " + element.getClass().getName() +
-            //    " @ [" + nodeStartOffset + "->" + nodeEndOffset + ']');
+			// If this node contains the span of interest then record it
+			if (nodeStartOffset <= fStartOffset && nodeEndOffset >= fEndOffset) {
+				//System.out.println("LEGNodeLocator.NodeVisitor.preVisit(ASTNode) SELECTED for offsets [" + fStartOffset + ".." + fEndOffset + "]");
+				fNode[0] = element;
+				return true; // to continue visiting here?
+			}
+			return false; // to stop visiting here?
+		}
+	}
 
-            // If this node contains the span of interest then record it
-            if (nodeStartOffset <= fStartOffset && nodeEndOffset >= fEndOffset) {
-                //System.out.println("$LANG_NAME$NodeLocator.NodeVisitor.preVisit($AST_NODE$) SELECTED for offsets [" + fStartOffset + ".." + fEndOffset + "]");
-                fNode[0]= element;
-                return true; // to continue visiting here?
-            }
-            return false; // to stop visiting here?
-        }
-    }
+	private NodeVisitor fVisitor = new NodeVisitor();
 
-    private NodeVisitor fVisitor= new NodeVisitor();
+	public Object findNode(Object ast, int offset) {
+		return findNode(ast, offset, offset);
+	}
 
-    public Object findNode(Object ast, int offset) {
-        return findNode(ast, offset, offset);
-    }
+	public Object findNode(Object ast, int startOffset, int endOffset) {
+		// System.out.println("Looking for node spanning offsets " + startOffset + " => " + endOffset);
+		fStartOffset = startOffset;
+		fEndOffset = endOffset;
+		// The following could be treated as an IASTNodeToken, but ASTNode
+		// is required for the visit/preVisit method, and there's no reason
+		// to use both of those types
+		((ASTNode) ast).accept(fVisitor);
+		if (fNode[0] == null) {
+			//System.out.println("Selected node:  null");
+		} else {
+			//System.out.println("Selected node: " + fNode[0] + " [" +
+			//   fNode[0].getLeftIToken().getStartOffset() + ".." + fNode[0].getLeftIToken().getEndOffset() + "]");
+		}
+		return fNode[0];
+	}
 
-    public Object findNode(Object ast, int startOffset, int endOffset) {
-        // System.out.println("Looking for node spanning offsets " + startOffset + " => " + endOffset);
-        fStartOffset = startOffset;
-        fEndOffset = endOffset;
-        // The following could be treated as an I$AST_NODE$Token, but $AST_NODE$
-        // is required for the visit/preVisit method, and there's no reason
-        // to use both of those types
-        ((IAst) ast).accept(fVisitor);
-        if (fNode[0] == null) {
-            //System.out.println("Selected node:  null");
-        } else {
-            //System.out.println("Selected node: " + fNode[0] + " [" +
-            //   fNode[0].getLeftIToken().getStartOffset() + ".." + fNode[0].getLeftIToken().getEndOffset() + "]");
-        }
-        return fNode[0];
-    }
+	public int getStartOffset(Object entity) {
+                if (entity instanceof IAst) {
+                    IAst n= (IAst) entity;
+                    return n.getLeftIToken().getStartOffset();
+                } else if (entity instanceof IToken) {
+                    IToken t= (IToken) entity;
+                    return t.getStartOffset();
+                }
+                return 0;
+	}
 
-    public int getStartOffset(Object node) {
-        IAst n = (IAst) node;
-        return n.getLeftIToken().getStartOffset();
-    }
+	public int getEndOffset(Object entity) {
+                if (entity instanceof IAst) {
+                    IAst n= (IAst) entity;
+                    return n.getLeftIToken().getEndOffset();
+                } else if (entity instanceof IToken) {
+                    IToken t= (IToken) entity;
+                    return t.getEndOffset();
+                }
+                return 0;
+	}
 
-    public int getEndOffset(Object node) {
-        IAst n = (IAst) node;
-        return n.getRightIToken().getEndOffset();
-    }
+	public int getLength(Object entity) {
+		return getEndOffset(entity) - getStartOffset(entity);
+	}
 
-    public int getLength(Object  node) {
-        IAst n = (IAst) node;
-        return getEndOffset(n) - getStartOffset(n);
-    }
-
-    public IPath getPath(Object node) {
-        // TODO Determine path of compilation unit containing this node
-        return new Path("");
-    }
+	public IPath getPath(Object node) {
+		// TODO Determine path of compilation unit containing this node
+		return new Path("");
+	}
 }
--- templates/ParseController.java
+++ templates/ParseController.java
@@ -2,15 +2,16 @@ package $PACKAGE_NAME$;
 
 import java.io.IOException;
 
-import lpg.runtime.IMessageHandler;
-
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.imp.language.ILanguageSyntaxProperties;
 import org.eclipse.imp.model.ISourceProject;
-import org.eclipse.imp.parser.IASTNodeLocator;
 import org.eclipse.imp.parser.ILexer;
+import org.eclipse.imp.parser.IMessageHandler;
 import org.eclipse.imp.parser.IParseController;
 import org.eclipse.imp.parser.IParser;
+import org.eclipse.imp.parser.ISourcePositionLocator;
+import org.eclipse.imp.parser.MessageHandlerAdapter;
 import org.eclipse.imp.parser.SimpleLPGParseController;
 
 import $AST_PKG_NODE$;
@@ -25,82 +26,83 @@ public class $CLASS_NAME_PREFIX$ParseController
     extends SimpleLPGParseController
     implements IParseController
 {
-    private $PARSER_TYPE$ parser;
-    private $LEXER_TYPE$ lexer;
+	private $CLASS_NAME_PREFIX$Parser parser;
 
-    private char keywords[][];
-    private boolean isKeyword[];
+	private $CLASS_NAME_PREFIX$Lexer lexer;
 
-    /**
-     * @param filePath		Project-relative path of file
-     * @param project		Project that contains the file
-     * @param handler		A message handler to receive error messages (or any others)
-     * 						from the parser
-     */
-    private IPath fullFilePath;
-    public void initialize(IPath filePath, ISourceProject project, IMessageHandler handler) {
-        super.initialize(filePath, project, handler);
-        fullFilePath = project.getRawProject().getLocation().append(filePath);
-//        createLexerAndParser(fullFilePath);
-    }
+        public $CLASS_NAME_PREFIX$ParseController() { }
 
+	/**
+	 * @param filePath
+	 *            Absolute path of file
+	 * @param project
+	 *            Project that contains the file
+	 * @param handler
+	 *            A message handler to receive error messages (or any others)
+	 *            from the parser
+	 */
+	public void initialize(IPath filePath, ISourceProject project,
+			IMessageHandler handler) {
+		super.initialize(filePath, project, handler);
+	}
 
-    public IParser getParser() { return parser; }
-    public ILexer getLexer() { return lexer; }
+	public IParser getParser() {
+		return parser;
+	}
 
-    public IASTNodeLocator getNodeLocator() { return new $CLASS_NAME_PREFIX$ASTNodeLocator(); } //return new AstLocator(); }
-    
-    public $CLASS_NAME_PREFIX$ParseController()
-    {
-        lexer = new $LEXER_TYPE$();
-        parser = new $PARSER_TYPE$();
-    }
+	public ILexer getLexer() {
+		return lexer;
+	}
+	
 
-//    private void createLexerAndParser(IPath filePath) {
-//        try {
-//            lexer = new $LEXER_TYPE$(filePath.toOSString()); // Create the lexer
-//            parser = new $PARSER_TYPE$(lexer.getLexStream() /*, project*/);  // Create the parser
-//        } catch (IOException e) {
-//            throw new Error(e);
-//        }
-//    }
-    
-    /**
-     * setFilePath() should be called before calling this method.
-     */
-    public Object parse(String contents, boolean scanOnly, IProgressMonitor monitor)
-    {
-        PMMonitor my_monitor = new PMMonitor(monitor);
-        char[] contentsArray = contents.toCharArray();
+	public ISourcePositionLocator getNodeLocator() {
+		return new $CLASS_NAME_PREFIX$ASTNodeLocator();
+	}
 
-        lexer.reset(contentsArray, fullFilePath.toOSString()); // fFilePath.toPortableString() ?
-        parser.reset(lexer.getLexStream());
-        parser.getParseStream().setMessageHandler(handler);
+        public ILanguageSyntaxProperties getSyntaxProperties() {
+            return null;
+        }
 
-        // SMS 28 Mar 2007
-        // Commenting out to prevent clobbering of markers set by previous
-        // builders in the same build phase.  This will also give behavior
-        // that is more consistent with the handling of markers in the JDT.
-//        IResource file = project.getFile(filePath);
-//        try {
-//            file.deleteMarkers(IMarker.PROBLEM, true, IResource.DEPTH_INFINITE);
-//        } catch(CoreException e) {
-//        System.err.println("$LANG_NAME$ParseController.parse:  caught CoreException while deleting problem markers; continuing to parse regardless");
-//        }
-        
-        lexer.lexer(my_monitor, parser.getParseStream()); // Lex the stream to produce the token stream
-        if (my_monitor.isCancelled())
-            return fCurrentAst; // TODO fCurrentAst might (probably will) be inconsistent wrt the lex stream now
+        /**
+	 * setFilePath() should be called before calling this method.
+	 */
+	public Object parse(String contents, boolean scanOnly,
+			IProgressMonitor monitor) {
+		PMMonitor my_monitor = new PMMonitor(monitor);
+		char[] contentsArray = contents.toCharArray();
 
-        fCurrentAst = ($AST_NODE$) parser.parser(my_monitor, 0);
-        parser.resolve(($AST_NODE$)fCurrentAst);
+		if (lexer == null) {
+		  lexer = new $CLASS_NAME_PREFIX$Lexer();
+		}
+		lexer.reset(contentsArray, fFilePath.toPortableString());
+		
+		if (parser == null) {
+			parser = new $CLASS_NAME_PREFIX$Parser(lexer.getLexStream());
+		}
+		parser.reset(lexer.getLexStream());
+		parser.getParseStream().setMessageHandler(new MessageHandlerAdapter(handler));
 
-        cacheKeywordsOnce();
+		lexer.lexer(my_monitor, parser.getParseStream()); // Lex the stream to
+															// produce the token
+															// stream
+		if (my_monitor.isCancelled())
+			return fCurrentAst; // TODO fCurrentAst might (probably will) be
+								// inconsistent wrt the lex stream now
 
-        return fCurrentAst;
-    }
+		fCurrentAst = parser.parser(my_monitor, 0);
 
-    
- 
-    
+		// SMS 18 Dec 2007:  functionDeclarationList is a LEG-specific type
+		// not suitable for other languages; try replacing with ASTNode
+//		if (fCurrentAst instanceof functionDeclarationList) {
+//			parser.resolve((ASTNode) fCurrentAst);
+//		}
+		if (fCurrentAst instanceof ASTNode) {
+			parser.resolve((ASTNode) fCurrentAst);
+		}
+		
+		cacheKeywordsOnce();
+
+		Object result = fCurrentAst;
+		return result;
+	}
 }
