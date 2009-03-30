9768ad9e89766c3bb2b14dc22c610e8c1c7b6653
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-30 17:27:09 +0000

    Pulled implementation of getParser() and getLexer() up to SimpleLPGParseController, now that IParser and ILexer provide sufficient API for most needs. Removed local impl of getNodeLocator(), now that there's a single impl for all LPG-based parsers. Removed unnecessary override of initialize(). Accommodated change to IParseController.parse(): no longer takes the dummy/bogus argument "scanOnly".

--- templates/ParseController.java
+++ templates/ParseController.java
@@ -20,74 +20,38 @@ import $AST_PKG_NODE$;
  */
 public class $CLASS_NAME_PREFIX$ParseController
     extends SimpleLPGParseController
-    implements IParseController
 {
-	private $CLASS_NAME_PREFIX$Parser parser;
-
-	private $CLASS_NAME_PREFIX$Lexer lexer;
-
     public $CLASS_NAME_PREFIX$ParseController() {
     	super($PLUGIN_CLASS$.kLanguageID);
     }
 
+	public ILanguageSyntaxProperties getSyntaxProperties() {
+	    return null;
+	}
+
 	/**
-	 * @param filePath
-	 *            Absolute path of file
-	 * @param project
-	 *            Project that contains the file
-	 * @param handler
-	 *            A message handler to receive error messages (or any others)
-	 *            from the parser
-	 */
-	public void initialize(IPath filePath, ISourceProject project,
-			IMessageHandler handler) {
-		super.initialize(filePath, project, handler);
-	}
-
-	public IParser getParser() {
-		return parser;
-	}
-
-	public ILexer getLexer() {
-		return lexer;
-	}
-	
-
-	public ISourcePositionLocator getNodeLocator() {
-		return new $CLASS_NAME_PREFIX$SourcePositionLocator();
-	}
-
-        public ILanguageSyntaxProperties getSyntaxProperties() {
-            return null;
-        }
-
-        /**
 	 * setFilePath() should be called before calling this method.
 	 */
-	public Object parse(String contents, boolean scanOnly,
-			IProgressMonitor monitor) {
+	public Object parse(String contents, IProgressMonitor monitor) {
 		PMMonitor my_monitor = new PMMonitor(monitor);
 		char[] contentsArray = contents.toCharArray();
 
-		if (lexer == null) {
-		  lexer = new $CLASS_NAME_PREFIX$Lexer();
+		if (fLexer == null) {
+		  fLexer = new $CLASS_NAME_PREFIX$Lexer();
 		}
-		lexer.reset(contentsArray, fFilePath.toPortableString());
+		fLexer.reset(contentsArray, fFilePath.toPortableString());
 		
-		if (parser == null) {
-			parser = new $CLASS_NAME_PREFIX$Parser(lexer.getLexStream());
+		if (fParser == null) {
+			fParser = new $CLASS_NAME_PREFIX$Parser(fLexer.getILexStream());
 		}
-		parser.reset(lexer.getLexStream());
-		parser.getParseStream().setMessageHandler(new MessageHandlerAdapter(handler));
+		fParser.reset(fLexer.getILexStream());
+		fParser.getIPrsStream().setMessageHandler(new MessageHandlerAdapter(handler));
 
-		lexer.lexer(my_monitor, parser.getParseStream()); // Lex the stream to
-															// produce the token
-															// stream
+		fLexer.lexer(my_monitor, fParser.getIPrsStream()); // Lex the stream to produce the token stream
 		if (my_monitor.isCancelled())
-			return fCurrentAst; // TODO fCurrentAst might (probably will) be
-								// inconsistent wrt the lex stream now
+			return fCurrentAst; // TODO fCurrentAst might (probably will) be inconsistent wrt the lex stream now
 
-		fCurrentAst = parser.parser(my_monitor, 0);
+		fCurrentAst = fParser.parser(my_monitor, 0);
 
 		// SMS 18 Dec 2007:  functionDeclarationList is a LEG-specific type
 		// not suitable for other languages; try replacing with ASTNode
@@ -95,12 +59,11 @@ public class $CLASS_NAME_PREFIX$ParseController
 //			parser.resolve((ASTNode) fCurrentAst);
 //		}
 		if (fCurrentAst instanceof ASTNode) {
-			parser.resolve((ASTNode) fCurrentAst);
+			(($CLASS_NAME_PREFIX$Parser) parser).resolve((ASTNode) fCurrentAst);
 		}
 		
 		cacheKeywordsOnce();
 
-		Object result = fCurrentAst;
-		return result;
+		return fCurrentAst;
 	}
 }
